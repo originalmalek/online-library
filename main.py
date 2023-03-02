08bff14e1b71e6a0e5dbde87561ea2f9b4dfa889
originalmalek <originalmalek@gmail.com>
2023-03-02 19:09:43 -0300

    check for redirect

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ import requests
 import os
 
 
+def check_for_redirects(response):
+    if response.status_code != 200:
+        raise requests.HTTPError
+
+
 def download_book(url, book_id):
     url = url + str(book_id)
 
-    response = requests.get(url)
-    response.raise_for_status()
+    response = requests.get(url, allow_redirects=False)
 
-    filename = f'book{book_id}.txt'
-    with open(f'books/{filename}', 'wb') as file:
-        file.write(response.content)
+    try:
+        check_for_redirects(response)
+        filename = f'book{book_id}.txt'
+        with open(f'books/{filename}', 'wb') as file:
+            file.write(response.content)
+    except requests.HTTPError:
+        return False
+
 
 def downolad_books(url, start_id: int, end_id: int):
     os.makedirs('books', exist_ok=True)
