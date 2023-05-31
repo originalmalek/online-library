675d76d7a0467be3f2b6befea254a59a3c80d1cd
originalmalek <originalmalek@gmail.com>
2023-05-31 09:58:34 +0200

    bug fix with errors

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import re
+import sys
 from time import sleep
 from urllib.parse import urljoin
 
@@ -10,6 +11,21 @@ from environs import Env
 
 from parse_tululu_by_id import check_for_redirects, parse_book_page, download_txt_book, download_book_image
 from parse_tululu_by_id import save_books_file
+
+
+def get_last_category_page(category_url):
+    try:
+        response = requests.get(category_url)
+        response.raise_for_status()
+        check_for_redirects(response)
+    except requests.HTTPError:
+        print(f'Категории {category_id} не существует')
+        sys.exit(1)
+
+    soup = BeautifulSoup(response.text, 'lxml')
+    last_page_selector = 'a.npage'
+    last_page = int(soup.select(last_page_selector)[-1].text)
+    return last_page
 
 
 def main():
@@ -23,7 +39,7 @@ def main():
     parser.add_argument('-e', '--end_page',
                         help='Номер последней cтраницы парсинга',
                         type=int,
-                        default=702)
+                        default=None)
     parser.add_argument('-df', '--dest_folder',
                         help='путь к каталогу с результатами парсинга: картинкам, книгам, JSON',
                         type=str,
@@ -40,16 +56,12 @@ def main():
                         default='books.json')
     args = parser.parse_args()
 
-    start_page = args.start_page
+    category_page = args.start_page
     end_page = args.end_page
     destination_folder = args.dest_folder
     skip_images = args.skip_imgs
     skip_txt = args.skip_txt
     json_name = args.json_name
-
-    url = 'https://tululu.org/'
-
-    books = {}
 
     env = Env()
     env.read_env()
@@ -59,31 +71,38 @@ def main():
     books_file_name = os.path.join(destination_folder, json_name)
     category_id = env('CATEGORY_ID')
 
-    category_url = urljoin(url, category_id)
-
-    response = requests.get(category_url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
-    last_page_selector = 'a.npage'
-    last_page = int(soup.select(last_page_selector)[-1].text)
-
-    if end_page == None or end_page > last_page:
-        end_page = last_page
-
     os.makedirs(books_folder, exist_ok=True)
     os.makedirs(books_images_folder, exist_ok=True)
 
-    for category_page in range(start_page, end_page):
+    url = 'https://tululu.org/'
+    category_url = urljoin(url, f'{category_id}/')
 
-        response = requests.get(f'{category_url}/{category_page}')
-        response.raise_for_status()
+    books = {}
+
+    if end_page == None:
+        end_page = get_last_category_page(category_url)
+
+    while category_page <= end_page:
+        try:
+            response = requests.get(f'{category_url}/{category_page}')
+            response.raise_for_status()
+            check_for_redirects(response)
+        except requests.ConnectionError:
+            print('Ошибка соединения, следующая попытка через 60 секунд')
+            sleep(5)
+            continue
+        except requests.HTTPError:
+            print(f'Такой страницы в категории {category_id} не существует')
+            sys.exit(1)
+
         soup = BeautifulSoup(response.text, 'lxml')
 
         on_page_books_selector = 'div#content table.d_book'
         on_page_books = soup.select(on_page_books_selector)
 
-        for book in on_page_books:
-            book_id = re.search('\d+', book.select_one('a')['href'])[0]
+        on_page_book = 0
+        while on_page_book < len(on_page_books):
+            book_id = re.search('\d+', on_page_books[on_page_book].select_one('a')['href'])[0]
             payload = {'id': book_id}
             book_download_url = urljoin(url, 'txt.php')
             book_url = urljoin(url, f'b{book_id}/')
@@ -91,8 +110,8 @@ def main():
             try:
                 response_page = requests.get(book_url)
                 response_page.raise_for_status()
-
                 check_for_redirects(response_page)
+
                 book = parse_book_page(response_page)
 
                 if not skip_txt:
@@ -107,12 +126,14 @@ def main():
 
             except requests.ConnectionError:
                 print('Ошибка соединения, следующая попытка через 60 секунд')
-                sleep(60)
+                sleep(5)
                 continue
 
             except requests.HTTPError:
                 print(f'Книги с id {book_id} или описания к ней не существует')
 
+            on_page_book += 1
+        category_page += 1
     save_books_file(books_file_name, books)
 
 
