6b8db9ec7c24842b51f68a17072440d1cf22cd91
originalmalek <originalmalek@gmail.com>
2023-06-07 11:00:08 +0200

    Add paginator

--- render_website.py
+++ render_website.py
@@ -6,21 +6,22 @@ from more_itertools import grouper, chunked
 import os
 
 
-def render_page(book_image_folder, books, chunked_books, page_number, site_directory):
+def render_page(book_image_folder, books, chunked_books, page_number, site_directory, page_quantity):
     template = env.get_template('template.html')
 
     rendered_page = template.render(
         books = books,
         book_image_folder = book_image_folder,
-        chunked_books = chunked_books
-
+        chunked_books = chunked_books,
+        page_quantity = page_quantity,
+        page_number = page_number,
     )
-
+    print(page_quantity)
     with open(f'{site_directory}/index{page_number}.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
 
-def on_reload(site_directory):
+def on_reload():
     render_pages(books_file_name, book_image_folder, site_directory)
 
 
@@ -31,8 +32,11 @@ def render_pages(books_file_name, book_image_folder, site_directory):
     books = json.loads(books_json)
 
     chunked_books = list(chunked(grouper(books, 2, incomplete='fill', fillvalue='0'), 5))
+    page_quantity = len(chunked_books)
+    print()
+    print(page_quantity)
     for page_number, books_group in enumerate(chunked_books, start=1):
-        render_page(book_image_folder, books, books_group, page_number, site_directory)
+        render_page(book_image_folder, books, books_group, page_number, site_directory, page_quantity)
 
 
 
@@ -49,7 +53,7 @@ if __name__ == '__main__':
 
     os.makedirs(site_directory, mode=0o777, exist_ok=True)
 
-    on_reload(site_directory)
+    on_reload()
     server = Server()
     server.watch('template.html', on_reload)
     server.serve(root='.')
